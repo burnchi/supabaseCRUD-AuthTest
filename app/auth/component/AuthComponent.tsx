ebde319afff7cf45eff39e376320b4964599b674
burnchi <1273732363@qq.com>
2023-11-09 23:51:19 +0800

    config supabase dashboard redirect url

--- app/auth/component/AuthComponent.tsx
+++ app/auth/component/AuthComponent.tsx
@@ -8,25 +8,12 @@ const AuthComponent = () => {
     const supabase = createClientComponentClient();
     console.log()
 
-    const getURL = () => {
-        let url =
-          process?.env?.NEXT_PUBLIC_SITE_URL ?? // Set this to your site URL in production env.
-          process?.env?.NEXT_PUBLIC_VERCEL_URL ?? // Automatically set by Vercel.
-          'http://localhost:3000/'
-        // Make sure to include `https://` when not localhost.
-        url = url.includes('http') ? url : `https://${url}`
-        // Make sure to include a trailing `/`.
-        url = url.charAt(url.length - 1) === '/' ? url : `${url}/`
-        // console.log(url)
-        
-        return url
-      }
     const handleLogin = async() =>{
         await supabase.auth.signInWithOAuth({
             provider: "github",
-            options : {
-                redirectTo : `${getURL()}auth/callback`
-            }
+            // options : {
+            //     redirectTo : `${location.origin}/auth/callback`
+            // }
         })
     }
 
